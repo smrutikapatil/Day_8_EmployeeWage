92021407a56da3bd84f01ff1611ea3692ed83d1c
smrutikapatil <smruti.patil17@gmail.com>
2022-01-03 23:01:28 +0530

    Added- calculated empolyee wage using while loop

---
+++ EmpWageUsingWhile.java
@@ -0,0 +1,36 @@
+package bridgelabz.employeewageproblem;
+
+public class EmpWageUsingWhile {
+	public static final int is_fulltime_Present = 1;
+	public static final int is_parttime_Present = 2;
+	public static final int wage_pr_hr=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HOURS_FOR_MONTH = 100;
+	
+	public static void main(String[] args) {
+		getMonthlyEmpWage();
+    }
+	static void getMonthlyEmpWage() {
+		int empHrs= 0, totalEmpHrs= 0, totalWorkingDays=0;
+		//Computation
+		while(totalEmpHrs <= MAX_HOURS_FOR_MONTH &&
+			   totalWorkingDays < NUM_OF_WORKING_DAYS) { 
+			   totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10 ) %3;
+			switch(empCheck) {
+			case is_parttime_Present :
+				empHrs=4;
+				break;
+			case is_fulltime_Present:
+				empHrs=8;
+				break;
+			default:
+			empHrs=0;
+		}
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
+		}
+	int totalEmpWage = totalEmpHrs * wage_pr_hr;
+	System.out.println("Total Emp Wage: " + totalEmpWage);
+ }
+}
